dfe7d711e882350223ba57c23a6b4f2a08c10557
Luc Janssen <lphjanss@gmail.com>
2020-04-20 11:45:15 +0200

    VTT interprator, google translation

--- app/Http/Controllers/FileInterpretationController.php
+++ app/Http/Controllers/FileInterpretationController.php
@@ -3,23 +3,31 @@
 namespace App\Http\Controllers;
 
 use Carbon\Traits\Timestamp;
+use Dotenv\Result\Result;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Google\Cloud\Translate\V2\TranslateClient;
 
 class FileInterpretationController extends Controller
 {
-    public function EchoFile()
+    public function splitFile(Request $request)
     {
-        $result = array();
+        $validateData = $request->validate([
+            'fileLocation' => 'required',
+            'targetLanguage' => 'required',
+        ]);
+
+        $splitFile = array();
         $counter = 0;
-        $string = File::get(storage_path('app\files\text.vtt'));
-        $SplitTextBlock = explode((PHP_EOL.PHP_EOL), $string);  //split text into blocks
-        foreach ($SplitTextBlock as $block)                      
-        {
-            $result[$counter] = $this->splitStrings($block);    //split text blocks
+        $string = File::get(storage_path($validateData['fileLocation']));
+        $SplitTextBlock = explode((PHP_EOL . PHP_EOL), $string);  //split text into blocks
+        foreach ($SplitTextBlock as $block) {
+            $splitFile[$counter] = $this->splitStrings($block);    //split text blocks
             $counter++;
         }
-        var_dump($result);
+        //return $splitFile;
+        $result = $this->translateStrings($splitFile, $validateData['targetLanguage']);
+        return $result;
     }
 
     public function splitStrings(String $textBlock)
@@ -35,6 +43,23 @@ class FileInterpretationController extends Controller
             "Text" => $split[2],
             //"EmptyLine" => " \n"
         );
-        return $splitBlockResult; 
+        return $splitBlockResult;
+    }
+
+    public function translateStrings(array $splitFile, String $targetLanguage)
+    {
+        $Onlytext = array();
+        foreach ($splitFile as $splitBlockResult) {
+            $apiKey = 'AIzaSyDsuGQFqyB1948NB_ZyHt6w8W_ccX6AkBE';
+            $text = $splitBlockResult["Text"];
+            $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=en&target=' . $targetLanguage;
+            $handle = curl_init($url);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($handle);                 
+            $responseDecoded = json_decode($response, true);
+            curl_close($handle);
+            array_push($Onlytext, $responseDecoded["data"]["translations"]);
+        }
+        return $Onlytext;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', 'FileInterpretationController@EchoFile');
+Route::Post('/TranslateFile', 'FileInterpretationController@TranslateFile');
+Route::get('/test', 'FileInterpretationController@splitFile');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
