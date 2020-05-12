656abc7f14aa8a05c4cfe5ca26b1bf1c91b1312d
sjors <sjors.pol@outlook.com>
2020-05-12 10:36:54 +0200

    save file local and save path in db

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\File;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
     function index(Request $request)
     {
-        $filePath = $request->file('user_file')->store('uploads');
+        $filePath = $request->file('vtt')->store('uploads');
 
-        //$file = File::make(public_path("storage/{$filePath}"));
-        //$file-> saveindb();
+        $file = new File();
+        $file->filePath = $filePath;
+        $file->save();
         echo $filePath;
     }   
 }
