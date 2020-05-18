7eb1f5729c468ee153ac0a141f018ccf83439977
Luc Janssen <lphjanss@gmail.com>
2020-05-18 10:03:20 +0200

    Revert because of F upp

---
+++ app/Console/Commands/SplitFile.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\CommandVariables;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use phpDocumentor\Reflection\Types\Mixed_;
+use Podlove\Webvtt\Parser;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Http\Request;
+
+class SplitFile extends Command
+{
+    protected $filePath;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(string $FilePath) 
+    {
+        $this->filePath = $FilePath;
+        parent::__construct();
+     }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $parser = new Parser();
+        $content = File::get(storage_path($this->filePath));
+        $result = $parser->parse($content);
+        return $result;
+    }
+}
--- app/Http/Controllers/Api/FileInterpretationController.php
+++ app/Http/Controllers/Api/FileInterpretationController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Collection;
+use App\CommandVariables;
+use App\Console\Commands\SplitFile;
+use App\Console\Commands\TranslateStrings;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Podlove\Webvtt\Parser;
@@ -17,7 +21,11 @@ class FileInterpretationController extends Controller
             'fileName' => 'required',
         ]);
         $splitFile = $this->splitFile($validateData['filePath']); //split the vtt file in associative array //https://github.com/podlove/webvtt-parser
-        $translatedSplitVtt = $this->translateStrings($splitFile['cues'], $validateData['targetLanguage']); 
+        $data = array(
+        'splitFile' => $splitFile,
+        'targetLanguage' => $validateData['targetLanguage']
+        );
+        $translatedSplitVtt = $this->translateStrings($data); //translate string in api logic
         $implodedTranslatedVtt = "WebVTT \n\n";
         foreach ($translatedSplitVtt as $block) {
             $implodedTranslatedVtt .= (gmdate("H:i:s", $block["start"]) . ' ' . '-->' . ' ');
@@ -32,64 +40,13 @@ class FileInterpretationController extends Controller
         return $implodedTranslatedVtt;
     }
 
-    public function splitFile(String $filePath)
+    public function splitFile(string $filePath)
     {
-        $parser = new Parser();
-        $content = File::get(storage_path($filePath));
-        $result = $parser->parse($content);
-        return $result;
+        return ($this->dispatch(new SplitFile($filePath)));
     }
-
-    /*
-    public function splitFile(String $filePath)
-    {
-        $splitFile = array();
-        $counter = 0;
-        $string = File::get(storage_path($filePath));
-        $SplitTextBlock = explode((PHP_EOL . PHP_EOL), $string);  //split text into blocks
-        foreach ($SplitTextBlock as $block) {
-            $splitFile[$counter] = $this->splitStrings($block);    //split text blocks
-            $counter++;
-        }
-        return $splitFile;
-    }
-    */
     
-    /*
-    public function splitStrings(String $textBlock)
+    public function translateStrings(array $array)
     {
-        $split[0] = trim(explode(("\n"), $textBlock)[0]); //timestamp
-        $naamZin = explode(("\n"), $textBlock)[1]; //naam + zin
-        $split[1] = trim(explode((">"), $naamZin)[0] . ">"); //naam
-        $split[2] = trim(explode((">"), $naamZin)[1]); //zin + formatting
-
-        $splitBlockResult = array(
-            "Timestamp" => $split[0],
-            "Naam" => $split[1],
-            "Text" => $split[2],
-            "EmptyLine" => "\n"
-        );
-        return $splitBlockResult;
-    }
-    */
-    
-    public function translateStrings(array $splitFile, String $targetLanguage)
-    {
-        $splitVttResultTranslated = array();
-
-        foreach ($splitFile as $splitBlockResult) {
-            $apiKey = env("API_KEY", "");
-            $text = $splitBlockResult["text"];
-            $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=en&target=' . $targetLanguage;
-            $handle = curl_init($url);
-            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($handle);
-            $responseDecoded = json_decode($response, true);
-            curl_close($handle);
-            $splitBlockResult["text"] = $responseDecoded["data"]["translations"][0]["translatedText"];
-            array_push($splitVttResultTranslated, $splitBlockResult);
-        }
-        dd($splitVttResultTranslated);
-        return $splitVttResultTranslated;
+        return ($this->dispatch(new TranslateStrings($array)));
     }
 }
