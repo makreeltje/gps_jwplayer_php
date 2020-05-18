c0e17c5983777b17d375d0acf3c6706a525d2d37
daniel stegeman <d.stegeman@student.fontys.nl>
2020-05-18 10:14:30 +0200

    scaffholding for cue storage

---
+++ app/Cue.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cue extends Model
+{
+    //
+    protected $table="Cues";
+
+}
---
+++ app/Repositories/CueRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+
+//use Your Model
+
+/**
+ * Class CueRepository.
+ */
+class CueRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+    public function StoreCue(){
+
+    }
+    public function StoreVTT(){
+
+    }
+    public function GetCue(){
+
+    }
+    public function GetCuesForFile(int $fileId){
+
+    }
+    
+}
