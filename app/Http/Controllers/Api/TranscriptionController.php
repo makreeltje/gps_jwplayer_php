4e172c99437581e3fd89e41992a9e9d24de4fff5
daniel stegeman <d.stegeman@student.fontys.nl>
2020-05-18 09:53:50 +0200

    created table for cues

--- app/Http/Controllers/Api/TranscriptionController.php
+++ app/Http/Controllers/Api/TranscriptionController.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\Controller;
 class TranscriptionController extends Controller
 {
     public function TranscribeAudio(Request $request){
+
         $data = $request->input('filePath');
-        
         TranscriptionJob::dispatch($data,"nl-nl");
         return response("success");
     }
