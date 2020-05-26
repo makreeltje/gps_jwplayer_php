c0ee2d351bd2ff88764bad6c777bd4eb45cfd348
Luc Janssen <lphjanss@gmail.com>
2020-05-26 13:50:48 +0200

    punctuation fix

--- app/Http/Controllers/vttConstructor.php
+++ app/Http/Controllers/vttConstructor.php
@@ -13,8 +13,8 @@ class vttConstructor
             $startTime = (float)$block["start"];
             $endTime = (float)$block["end"];
 
-            $startMilSec = count(explode(",", $block["start"])) > 1 ? "." . explode(",", $block["start"])[1] . str_repeat("0", max(3 - strlen(explode(",", $block["start"])[1]), 0)) : ".000";
-            $endMilSec = count(explode(",", $block["end"])) > 1 ? "." . explode(",", $block["end"])[1] . str_repeat("0", max(3 - strlen(explode(",", $block["end"])[1]), 0)) : ".000";
+            $startMilSec = count(explode(".", $block["start"])) > 1 ? "." . explode(".", $block["start"])[1] . str_repeat("0", max(3 - strlen(explode(".", $block["start"])[1]), 0)) : ".000";
+            $endMilSec = count(explode(".", $block["end"])) > 1 ? "." . explode(".", $block["end"])[1] . str_repeat("0", max(3 - strlen(explode(".", $block["end"])[1]), 0)) : ".000";
 
             $implodedVtt .= (sprintf('%02d:%02d:%02d', ($startTime/3600),($startTime/60%60), $startTime%60) . $startMilSec . ' ' . '-->' . ' ');
             $implodedVtt .= (sprintf('%02d:%02d:%02d', ($endTime/3600),($endTime/60%60), $endTime%60) . $endMilSec . "\n");
