8d950de0a6b287a5acc31795641a6812026a0513
Freek Jansen <freek.jansen00@hotmail.com>
2020-06-09 14:02:29 +0200

    shoot me

--- app/Http/Controllers/Api/TranslationController.php
+++ app/Http/Controllers/Api/TranslationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class TranslationController extends Controller
