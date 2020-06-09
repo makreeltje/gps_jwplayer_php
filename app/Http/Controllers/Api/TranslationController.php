8d336607f2eac5a4fdcffa1761207764231fc420
Freek Jansen <freek.jansen00@hotmail.com>
2020-06-09 13:52:03 +0200

    Fixed another thing, sorry :/

--- app/Http/Controllers/Api/TranslationController.php
+++ app/Http/Controllers/Api/TranslationController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TranslationController extends Controller
 {
